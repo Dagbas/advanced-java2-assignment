e2ec642219513ee16a3e7422917090218ecb9026
Dagbas <135510844+Dagbas@users.noreply.github.com>
2023-09-05 20:11:43 -0400

    Update Product.java

--- src/genericsLambdas/Product.java
+++ src/genericsLambdas/Product.java
@@ -2,17 +2,22 @@ package genericsLambdas;
 
 public class Product {
 	//define the instance variables for 'name' and 'price'
+	private String name;
+	private double price;
 	
 
 	//define a public constructor for Product that takes a name and price as input and sets the instance variables accordingly
+	public Product ( String name, double price) {
+		this.name=name;
+		this.price=price;
+
+	}
+
 
 
 	public static void main(String[] args) {
 		//Make an Inventory object that holds Products
-
-		//Add as many products as you want to your Inventory Object using the addItem() method you defined 
-
-
+		//Add as many products as you want to your Inventory Object using the addItem() method you defined
 		//Your task is to calculate the total price of all products in an Inventory object called myInventory. Follow these steps:
 		//1. Call the getItems method on your inventory object to retrieve the list of all products.
 		//2. Convert this list into a stream by calling the stream method.
@@ -22,13 +27,16 @@ public class Product {
 		//4. Apply the sum operation to the stream to get the total price of all products.
 		//5. Store this total price in a double variable called priceTotal.
 		//6. Print out priceTotal to the console to make sure you are properly summing your Products!
+		public static void main(String[] args) {
+			Inventory<Product> myInventory = new Inventory<>();
+			myInventory.addItem(new Product("PS5 Console DISC Edition", 499.99));
+			myInventory.addItem(new Product("Gaming Chair", 249.99));
+			myInventory.addItem(new Product("4K Telivision", 699.99;
 
-		
-		
-		
-		//Helpful documentation:
-		//https://www.baeldung.com/java-stream-sum#using-streamsum-with-objects
+			double priceTotal = myInventory.getItems().stream().mapToDouble(product -> product.price).sum();
+
+			System.out.println(priceTotal);
+			System.out.println(myInventory.getItems());
+		}
 
 	}
-
-}
